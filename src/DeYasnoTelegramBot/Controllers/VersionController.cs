fcd8d8c4e346205f8c5e9131b61a86de318ca9cc
Roman Dzhykhur <rom3053@gmail.com>
2024-09-05 15:20:32 +0300

    .

--- src/DeYasnoTelegramBot/Controllers/VersionController.cs
+++ src/DeYasnoTelegramBot/Controllers/VersionController.cs
@@ -11,24 +11,45 @@ public class VersionController : ControllerBase
     public async Task<IActionResult> Get()
     {
         string htmlContent = @"
-        <!DOCTYPE html>
-        <html lang='en'>
-        <head>
-            <meta charset='UTF-8'>
-            <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-            <title>Test deploy</title>
-        </head>
-        <body>
-            <h1>AutoPlay YouTube Video</h1>
+    <!DOCTYPE html>
+    <html lang='en'>
+    <head>
+        <meta charset='UTF-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <style>
+            .videoContainer {
+                position: absolute;
+                width: 100%;
+                height: 100%;
+                top: 0;
+                left: 0;
+                bottom: 0;
+                right: 0;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+
+            iframe {
+                width: 100%;
+                height: 100%; 
+            }
+        </style>
+    </head>
+    <body>
+        <div class='videoContainer'>
             <iframe width='560' height='315' 
-                src='https://www.youtube.com/embed/XGxIE1hr0w4?&autoplay=1&t=20' 
+                src='https://www.youtube.com/embed/XGxIE1hr0w4?autoplay=1&start=17' 
                 frameborder='0' 
                 allow='autoplay; encrypted-media' 
                 allowfullscreen>
             </iframe>
-        </body>
-        </html>";
+        </div>
+    </body>
+    </html>";
 
         return Content(htmlContent, "text/html");
     }
+
 }
