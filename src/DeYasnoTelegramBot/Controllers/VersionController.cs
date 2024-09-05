9e1e2d8de965524b4098d8f8e95dd1b1a8e5f49f
Roman Dzhykhur <rom3053@gmail.com>
2024-09-05 16:49:11 +0300

    .

--- src/DeYasnoTelegramBot/Controllers/VersionController.cs
+++ src/DeYasnoTelegramBot/Controllers/VersionController.cs
@@ -10,47 +10,6 @@ public class VersionController : ControllerBase
     [HttpGet]
     public async Task<IActionResult> Get()
     {
-        string htmlContent = @"
-    <!DOCTYPE html>
-    <html lang='en'>
-    <head>
-        <meta charset='UTF-8'>
-        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
-        <style>
-            .videoContainer {
-                position: absolute;
-                width: 100%;
-                height: 100%;
-                top: 0;
-                left: 0;
-                bottom: 0;
-                right: 0;
-                display: flex;
-                flex-direction: column;
-                justify-content: center;
-                align-items: center;
-            }
-
-            iframe {
-                width: 100%;
-                height: 100%; 
-            }
-        </style>
-    </head>
-    <body>
-        <div class='videoContainer'>
-            <iframe width='560' height='315' 
-                src='https://www.youtube.com/embed/XGxIE1hr0w4?autoplay=1&start=17&enable_js=1' 
-                frameborder='0' 
-                allow='autoplay; encrypted-media' 
-                allowfullscreen>
-            </iframe>
-        </div>
-    </body>
-    </html>";
-
-        return Content(htmlContent, "text/html");
+        return Redirect("https://dn720407.ca.archive.org/0/items/rick-roll/Rick%20Roll.mp4");
     }
-
-
 }
